56dac3b38e3ff4156916ae3b758e7a80646db856
mattyo210 <41353376+mattyo210@users.noreply.github.com>
2018-10-09 00:01:38 +0900

    It can make scandata.txt!!

--- filetest.py
+++ filetest.py
@@ -1,10 +1,14 @@
 
 def scan_write(scanlist):
+	username=raw_input("input your user name\n")
 	string="".join(map(str, scanlist))+ "\n"
 	print(string)
-	f = open('scandata.txt','a')
+	filename="/home/"+ username +"/catkin_ws/src/robot/nodes/scandata.txt"
+	f = open(filename,'w')
    	f.write(string)
 	f.close()
 	print("fileclose")
 
-
+if __name__ == "__main__":
+    scanlist=[1.23,2.54,3.34,4.34,5.34,6.7777]
+    scan_write(scanlist)
